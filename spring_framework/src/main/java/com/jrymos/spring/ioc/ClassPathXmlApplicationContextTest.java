a1fe82a2b29a0a05e5f8511ad50c2f80eacb974a
zouweijun <zouweijun@joyy.com>
2025-04-09 14:32:58 +0800

    增加lifecycle学习

--- spring_framework/src/main/java/com/jrymos/spring/ioc/ClassPathXmlApplicationContextTest.java
+++ spring_framework/src/main/java/com/jrymos/spring/ioc/ClassPathXmlApplicationContextTest.java
@@ -3,15 +3,18 @@ package com.jrymos.spring.ioc;
 import com.jrymos.spring.ioc.bean.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ClassPathXmlApplicationContextTest {
 
     @Test
     public void test() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("ioc/user_service.xml");
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("ioc/user_service.xml");
+        context.start();
         UserService userService = context.getBean("userService", UserService.class);
         System.out.println(userService.getUsers());
+        context.registerShutdownHook();
     }
 
 }
--- spring_framework/src/main/java/com/jrymos/spring/ioc/bean/User.java
+++ spring_framework/src/main/java/com/jrymos/spring/ioc/bean/User.java
@@ -3,10 +3,20 @@ package com.jrymos.spring.ioc.bean;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.Lifecycle;
 
-public record User(Integer id, String name) implements InitializingBean, DisposableBean {
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public record User(Integer id, String name) implements InitializingBean, DisposableBean, Lifecycle {
     public User {
         System.out.println(this + "\t" + "User");
+    }
+
+
+    public void init() {
+        System.out.println(this + "\t" + "init");
     }
 
     @Override
@@ -23,4 +33,22 @@ public record User(Integer id, String name) implements InitializingBean, Disposa
     public void destroy() {
         System.out.println(this + "\t" + "destroy");
     }
+
+    @Override
+    public void start() {
+        System.out.println(this + "\t" + "start");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println(this + "\t" + "stop");
+        stops.add(this);
+    }
+
+    @Override
+    public boolean isRunning() {
+        return !stops.contains(this);
+    }
+
+    static Set<User> stops = new CopyOnWriteArraySet<>();
 }
